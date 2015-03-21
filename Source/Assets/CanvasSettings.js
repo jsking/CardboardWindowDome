b6408b8186f952084b4c19fd08180a8c28e35079
jsking <justinscottking@gmail.com>
2015-03-21 07:42:57 -0700

    Added Unity 4 Project

---
+++ Source/Assets/CanvasSettings.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var controlCanvas : Canvas;
+var settingsCanvas : Canvas;
+var networkCanvas : Canvas;
+var IPAddress : String = "0.0.0.0";
+var PortNum : String = "0000";
+
+function switchToNetworkCanvas () {
+	
+	networkCanvas.enabled = true;
+	settingsCanvas.enabled = false;
+}
+
+function switchToSettingsCanvas () {
+	
+	networkCanvas.enabled = false;
+	controlCanvas.enabled = false;
+	settingsCanvas.enabled = true;
+}
+
+function switchToControlCanvas () {
+	controlCanvas.enabled = true;
+	settingsCanvas.enabled = false;
+}
+
+function updateIPAddress (theNewNum) {
+	IPAddress = theNewNum;
+}
+
+function updatePort (theNewNum) {
+	PortNum = theNewNum;
+}
+
+function ConnectToServer () {
+	Network.Connect(IPAddress, parseInt(PortNum));
+}
