7b5d9d92731f0764895450d1eaa35ee5f656e20f
jsking <justinscottking@gmail.com>
2015-03-24 07:33:37 -0700

    Added window close button

--- Source/Assets/CameraScript.js
+++ Source/Assets/CameraScript.js
@@ -11,6 +11,7 @@ function Start () {
 
 function Update () {
 	var cardboard = (transform.parent.GetComponent(Cardboard) as Cardboard);
+	
 	if(Physics.Raycast(Vector3.zero, transform.forward, hit) && hit.collider.tag == "TitleBar") {
 		go = hit.collider.gameObject;
 		theMat.color = Color.green;
@@ -22,4 +23,11 @@ function Update () {
 		theMat.color = Color.white;
 	}
 	
+	if(Physics.Raycast(Vector3.zero, transform.forward, hit) && hit.collider.tag == "CloseButton" && cardboard.CardboardTriggered) {
+		iTween.ScaleTo(hit.collider.transform.parent.gameObject, {"scale": Vector3.zero, "time": 0.5f, "oncomplete": "DestroyTheWindow"});
+	}
+}
+
+function DestroyTheWindow() {
+	Destroy(hit.collider.transform.parent.gameObject);
 }
