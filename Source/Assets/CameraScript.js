3d760a36927fd4323a92ceaf25344f326c82d164
jsking <justinscottking@gmail.com>
2015-03-25 21:16:55 -0700

    Added color picker

--- Source/Assets/CameraScript.js
+++ Source/Assets/CameraScript.js
@@ -4,14 +4,19 @@ var hit : RaycastHit;
 var go : GameObject;
 var theMat : Material;
 var foundIt = false;
+var curColorPicker : GameObject;
+var colorPickerRenderers : MeshRenderer[];
+var ColorRenderTex : RenderTexture;
+var colorPickerCurColor : Color;
+var crosshairs : Transform;
+var cardboard : Cardboard;
 
 function Start () {
 
 }
 
 function Update () {
-	var cardboard = (transform.parent.GetComponent(Cardboard) as Cardboard);
-	
+	cardboard = (transform.parent.GetComponent(Cardboard) as Cardboard);
 	if(Physics.Raycast(Vector3.zero, transform.forward, hit) && hit.collider.tag == "TitleBar") {
 		go = hit.collider.gameObject;
 		theMat.color = Color.green;
@@ -24,10 +29,49 @@ function Update () {
 	}
 	
 	if(Physics.Raycast(Vector3.zero, transform.forward, hit) && hit.collider.tag == "CloseButton" && cardboard.CardboardTriggered) {
-		iTween.ScaleTo(hit.collider.transform.parent.gameObject, {"scale": Vector3.zero, "time": 0.5f, "oncomplete": "DestroyTheWindow"});
+		Destroy(hit.collider.transform.parent.gameObject);
 	}
-}
-
-function DestroyTheWindow() {
-	Destroy(hit.collider.transform.parent.gameObject);
+	
+	if(Physics.Raycast(Vector3.zero, transform.forward, hit) && hit.collider.tag == "ColorField" && cardboard.CardboardTriggered) {
+		for(var cpr : MeshRenderer in colorPickerRenderers) {
+			cpr.enabled = true;
+			try {
+				cpr.GetComponent.<Collider>().enabled = true;
+			} catch(err) {
+				
+			}
+			try {
+				cpr.transform.parent.GetComponent.<Collider>().enabled = true;
+			} catch(err) {
+				
+			}
+		}
+		curColorPicker = hit.collider.gameObject;
+	}
+	
+	if(Physics.Raycast(Vector3.zero, transform.forward, hit) && hit.collider.tag == "ColorPicker" && cardboard.CardboardTriggered) {
+		crosshairs.rotation = transform.rotation;
+		var tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
+ 		RenderTexture.active = ColorRenderTex;
+ 		tex.ReadPixels(new Rect(128, 128, 384, 384), 0, 0);
+		RenderTexture.active = null;
+		colorPickerCurColor = tex.GetPixel(120, 120);
+	}
+	
+	if(Physics.Raycast(Vector3.zero, transform.forward, hit) && hit.collider.tag == "ColorPickerOK" && cardboard.CardboardTriggered) {
+		for(var cpr : MeshRenderer in colorPickerRenderers) {
+			cpr.enabled = false;
+			try {
+				cpr.GetComponent.<Collider>().enabled = false;
+			} catch(err) {
+				
+			}
+			try {
+				cpr.transform.parent.GetComponent.<Collider>().enabled = false;
+			} catch(err) {
+				
+			}
+		}
+		(curColorPicker.GetComponent(MaterialPicker) as MaterialPicker).colorPickerChangeMat.color = colorPickerCurColor;
+	}
 }
---
+++ Source/Assets/Standard Assets/MaterialPicker.js
@@ -0,0 +1,1 @@
+﻿var colorPickerChangeMat : Material;
